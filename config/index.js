a40b56b9eaa87a340c27e01882094741e061198a
dls <2045039125@qq.com>
2019-06-12 09:49:37 +0800

    打包、代码规则

--- config/index.js
+++ config/index.js
@@ -9,13 +9,14 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
+    // assetsPublicPath: './', // 打包
     assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
+    autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
@@ -26,7 +27,7 @@ module.exports = {
     useEslint: true,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
-    showEslintErrorsInOverlay: false,
+    showEslintErrorsInOverlay: true,
 
     /**
      * Source Maps
@@ -50,13 +51,14 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
+    // assetsPublicPath: './', // 打包
     assetsPublicPath: '/',
 
     /**
      * Source Maps
      */
 
-    productionSourceMap: true,
+    productionSourceMap: false,
     // https://webpack.js.org/configuration/devtool/#production
     devtool: '#source-map',
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{formData.name}}</div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+      formData: {
+        name: '15386',
+      },
+    };
+  },
+};
+</script>
